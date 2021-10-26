2e840afc57d783033e65307c1b2fedc6235c9b20
SerheyBernatovich <sec1982@ukr.net>
2021-10-26 18:48:34 +0300

    add

--- JSfull/lesson4/task9/index.js
+++ JSfull/lesson4/task9/index.js
@@ -5,5 +5,5 @@ for (let n = 1; n <= 1000; n += 1) {
 const div = sum / 1234;
 const mods = sum % 1234;
 
-const rezult = div > mods ? true : false;
+const rezult = true ? div > mods : mods > div;
 console.log(rezult);
