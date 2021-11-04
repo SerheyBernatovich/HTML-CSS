cf5b1f8853152f6a382051812c46cdbeacf91644
SerheyBernatovich <sec1982@ukr.net>
2021-11-04 16:31:14 +0200

    add

--- JSfull/lesson8/task7/index.js
+++ JSfull/lesson8/task7/index.js
@@ -1,15 +1,14 @@
-const pickProps = (obj, props) => {
-  let newObj = {};
-
-  for (let key in obj) {
-    props.forEach((element) => {
-      if (key === element) {
-        newObj[element] = obj[element];
-      }
-    });
+const getAdults = (obj) => {
+  const newObj = {};
+  for (let age in obj) {
+    // console.log(obj[age]);
+    if (obj[age] >= 18) {
+      newObj[age] = obj[age];
+    }
   }
   return newObj;
 };
 
-console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c'])); // ==> { a: 1, c: 3 }
-console.log(pickProps({ a: 1, b: 2, c: 3 }, ['a', 'c', 'd', 'hex'])); // ==> { a: 1, c: 3 }
+// examples
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
+console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
