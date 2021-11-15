49034f4f6f33646111b5f4b18c47bc6f0aef53de
SerheyBernatovich <sec1982@ukr.net>
2021-11-15 15:41:23 +0200

    add

---
+++ JSfull/lesson9/task4/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable prefer-object-spread */
+/* eslint-disable no-param-reassign */
+
+/* В решения этой задачи используется метод Object.assign. В реальных проектах для такой задачи
+ * лучше использовать spread опертор - это самый современный подход
+ *
+ * Так же плохой подход - мутировать входящие параметры функции
+ *
+ * Задачу мы делаем для практики и демонстрационных целей, поэтому чтобы eslint не ругался на эту ошибку,
+ * для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+function addPropertyV1(userData, userId) {}
+
+// put your code here
+
+// put your code here
+
+// put your code here
+
+// examples
+const user = {
+  name: 'Sam',
+};
+addPropertyV1(user, '1234567'); // ==> { name: 'Sam', id: '1234567' }
