b11278b797b1b202cfc6981650d2e570208f0212
SerheyBernatovich <sec1982@ukr.net>
2021-11-17 15:59:21 +0200

    add

---
+++ Coding/js/day2/index.js
@@ -0,0 +1,13 @@
+// algo
+// 1 iterate numbers from 2 to num
+// 2 iterate numbers from 2 to i
+// 3
+// 4
+function getPrimes(num) {
+  for (let i = 2; i <= num; i += 1) {
+    for (let number = 2; number < i; number += 1) {
+      if (i % number === 0) {
+      }
+    }
+  }
+}
