ef449bc20461e3b6f9a028f21890f3e76bd6ff1a
SerheyBernatovich <sec1982@ukr.net>
2021-11-18 12:51:08 +0200

    add

--- Coding/js/day2/index.js
+++ Coding/js/day2/index.js
@@ -3,11 +3,27 @@
 // 2 iterate numbers from 2 to i
 // 3
 // 4
+// input: num;
+// output: boolean;
+function isPrime(number) {
+  for (let index = 2; index < number; index += 1) {
+    // console.log('inner number' + number);
+    if (number % index === 0) {
+      // console.log('number' + i + 'is not prime');
+      return false;
+    }
+  }
+  return true;
+}
+
+// input: num
+// output: undefined
 function getPrimes(num) {
   for (let i = 2; i <= num; i += 1) {
-    for (let number = 2; number < i; number += 1) {
-      if (i % number === 0) {
-      }
+    if (isPrime(i)) {
+      console.log(i);
     }
   }
 }
+
+getPrimes(15);
