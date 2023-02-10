2b7259359e07cd4383ebd0b015a861f9dc7b199b
SerheyBernatovich <sec1982@ukr.net>
2023-02-10 09:50:05 +0200

    add

--- JSfull/14Lexical_environment_scope/task1/index.js
+++
@@ -1,8 +0,0 @@
-let message = 'Just learn it';
-
-export function sendMessage(name) {
-  console.log(`${name}, ${message}! Your Gromcode`);
-}
-export function setMessage(text) {
-  message = text;
-}
