0a1229773e4eca7cd1468c9df23dce2fe44a50ec
SerheyBernatovich <sec1982@ukr.net>
2023-03-30 09:36:57 +0300

    add

--- JSfull/19Prototypes.Inheritance/task2/index.js
+++
@@ -1,9 +0,0 @@
-export function getOwnProps(obj) {
-  const arr = [];
-  for (let i in obj) {
-    if (typeof obj[i] !== 'function' && obj.hasOwnProperty(i)) {
-      arr.push(i);
-    }
-  }
-  return arr;
-}
