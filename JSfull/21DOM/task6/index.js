af44946502d0f2c040efa4b6ffe472a4b677ee29
SerheyBernatovich <sec1982@ukr.net>
2023-05-18 07:58:15 +0300

    add

--- JSfull/21DOM/task6/index.js
+++
@@ -1,5 +0,0 @@
-export function setButton(text) {
-  document.querySelector('body').innerHTML = `<button>${text}</button>`;
-  // document.querySelector("body").textContent = `<button>${text}</button>`;
-}
-setButton('tee');
