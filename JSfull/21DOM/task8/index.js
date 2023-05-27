0ee977fb30e2fc0739526fb881b260228d341ed1
SerheyBernatovich <sec1982@ukr.net>
2023-05-27 09:08:05 +0300

    add

--- JSfull/21DOM/task8/index.js
+++
@@ -1,6 +0,0 @@
-export function createButton(texto) {
-  const button = document.createElement('button');
-  button.textContent = texto;
-  document.querySelector('body').append(button);
-}
-createButton('qqq');
