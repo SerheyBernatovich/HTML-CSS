724c107abaccbc17e33f8ba246fea25aa2fc3c1b
SerheyBernatovich <sec1982@ukr.net>
2023-06-15 14:01:48 +0300

    add

--- JSfull/21DOM/task12/index.js
+++
@@ -1,6 +0,0 @@
-export function squaredNumbers() {
-  document.querySelectorAll('.number').forEach(el => {
-    el.dataset.squaredNumber = el.dataset.number * el.dataset.number;
-  });
-}
-squaredNumbers();
