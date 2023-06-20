e20b6a0c48432c9163ef359ab75391313c117a32
SerheyBernatovich <sec1982@ukr.net>
2023-06-20 07:04:41 +0300

    add

--- JSfull/21DOM/task13/index.js
+++
@@ -1,5 +0,0 @@
-export function getSection(num) {
-  const el = document.querySelector(`span[data-number='${num}']`);
-  return el.closest('.box').getAttribute('data-section');
-}
-// getSection(6);
