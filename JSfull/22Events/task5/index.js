caa99293080af89cc8fd671bf12a96a88c0cdbb3
SerheyBernatovich <sec1982@ukr.net>
2023-07-07 10:23:37 +0300

    add

--- JSfull/22Events/task5/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.search__btn');
-const input = document.querySelector('.search__input');
-const clic = () => {
-  console.log(input.value);
-};
-
-button.addEventListener('click', clic);
