abc90144dc611c8be3985a50753b01eab7469292
SerheyBernatovich <sec1982@ukr.net>
2023-07-08 08:45:59 +0300

    add

--- JSfull/22Events/task6/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.single-use-btn');
-
-const clic = () => {
-  console.log('clicked');
-  button.removeEventListener('click', clic);
-};
-
-button.addEventListener('click', clic);
