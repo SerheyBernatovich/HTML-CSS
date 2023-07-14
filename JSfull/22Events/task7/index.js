9bac16915bb820f3e7cdebb31a5c5c5c7a609061
SerheyBernatovich <sec1982@ukr.net>
2023-07-14 10:13:52 +0300

    add

--- JSfull/22Events/task7/index.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelectorAll('.btn');
-
-const handleClick = event => {
-  console.log(event.target.textContent);
-};
-
-button[0].addEventListener('click', handleClick);
-button[1].addEventListener('click', handleClick);
