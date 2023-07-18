d4f6cb5069f6d070a26ebe4a5ec8e6d97606aa55
SerheyBernatovich <sec1982@ukr.net>
2023-07-18 09:40:17 +0300

    add

--- JSfull/22Events/task9/index.js
+++
@@ -1,7 +0,0 @@
-const checkbox = document.querySelector('.task-status');
-
-const handleClick = event => {
-  console.log(event.target.checked);
-};
-
-checkbox.addEventListener('change', handleClick);
