26d7c5bd530742afd71536d389be14a69f0054d8
SerheyBernatovich <sec1982@ukr.net>
2023-07-21 08:10:54 +0300

    add

--- JSfull/22Events/task10/index.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.pagination__page');
-
-const handleClick = event => {
-  console.log(event.target.textContent);
-};
-
-buttons[0].addEventListener('click', handleClick);
-buttons[1].addEventListener('click', handleClick);
-buttons[2].addEventListener('click', handleClick);
